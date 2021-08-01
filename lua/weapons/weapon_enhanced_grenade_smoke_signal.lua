e29a7d237aa321c333f366b98344ce5d44b16921
zrauf <zrauf@sfu.ca>
2021-08-01 12:48:58 -0700

    Add grenades to own weapon category

--- lua/weapons/weapon_enhanced_grenade_smoke_signal.lua
+++ lua/weapons/weapon_enhanced_grenade_smoke_signal.lua
@@ -2,6 +2,7 @@ SWEP.Base = "weapon_enhanced_grenade_base"
 SWEP.PrintName			= "Smoke Signal Grenade" -- This will be shown in the spawn menu, and in the weapon selection menu
 SWEP.Instructions		= "WIP"
 
+SWEP.Category = "Enhanced Grenades"
 SWEP.Spawnable = true
 
 SWEP.GrenadeEntity = "ent_enhanced_grenade_smoke_signal"
--- lua/weapons/weapon_enhanced_grenade_thermite.lua
+++ lua/weapons/weapon_enhanced_grenade_thermite.lua
@@ -2,6 +2,7 @@ SWEP.Base = "weapon_enhanced_grenade_base"
 SWEP.PrintName			= "Thermite Greande" -- This will be shown in the spawn menu, and in the weapon selection menu
 SWEP.Instructions		= "WIP"
 
+SWEP.Category = "Enhanced Grenades"
 SWEP.Spawnable = true
 
 SWEP.GrenadeEntity = "ent_enhanced_grenade_thermite"
