8e45ce64ae257ac5adec7d8be20dd0554a503f55
zrauf <zrauf@sfu.ca>
2021-08-14 23:14:33 -0700

    Remove uneccesary sound from base entity

--- lua/entities/ent_enhanced_grenade_base/shared.lua
+++ lua/entities/ent_enhanced_grenade_base/shared.lua
@@ -3,7 +3,6 @@ ENT.Base = "base_gmodentity"
 ENT.PrintName = "Base Greande Entity"
 ENT.Spawnable = false
 ENT.TickSound = Sound( "weapons/grenade/tick1.wav" )
-ENT.SmokeSound = Sound( "npc/env_headcrabcanister/hiss.wav" )
 ENT.SoundTimer = CurTime() + 4.5
 ENT.GrenadeLight = Material("sprites/light_glow02_add")
 ENT.GrenadeColor = Color(0, 255, 0)
